71ee8264eb8cb0d357f50a03374eba2954e2aa6c
Kieran Nehil-Puleo <kierannp@head.cl.vanderbilt.edu>
2023-07-25 22:58:40 -0500

    Changed file structure and made the package installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='mData',
+      version='1.0',
+      description='A package for multi-input graph data',
+      author='Kieran NP',
+      author_email='nehilkieran@gmail.com',
+      packages=['mData'],
+     )
